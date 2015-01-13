8985f7406b26ebc4eab942909fa2b878cb9d8cbc
Paul Day <paul@paulday.com.au>
2015-01-13 11:51:18 +1100

    gulp vars updated

--- gulp/clean.js
+++ gulp/clean.js
@@ -1,5 +1,4 @@
 module.exports = function(gulp, vars, $, args) {
-	console.log('clean vars', vars.root);
 gulp.task('clean', function () {  
   return gulp.src(vars.root, {read: false})
     .pipe($.clean({force: true}));
